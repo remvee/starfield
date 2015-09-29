d822890c5739550522d0bfd8b64f3e60ae804a79
Remco van 't Veer <remco@remworks.net>
2015-09-29 14:44:25 +0200

    Jelles bugs

--- src/starfield/core.cljs
+++ src/starfield/core.cljs
@@ -3,32 +3,45 @@
             [reagent.core :as r]))
 
 (def ^:const magic 50000)
-(def stars (r/atom (->> (fn [] {:x (rand-int magic)
-                                :y (rand-int magic)
-                                :z (rand-int (* 4 magic))})
-                        repeatedly (take 100))))
+(defn rand-image []
+  (rand-nth ["mug.png" "lieveheers.png"]))
+(defn rand-class []
+  (rand-nth ["cw" "ccw" "cw-slow" "ccw-slow" "cw-fast" "ccw-fast"]))
+
+(defn new-star []
+  {:x (rand-int magic)
+   :y (rand-int magic)
+   :z (rand-int (* 4 magic))
+   :image (rand-image)
+   :class (rand-class)})
+
+(def stars (r/atom (->> new-star repeatedly (take 100))))
 
 (defn move [stars]
   (for [{:keys [z] :as star} stars]
     (if (< z 0)
-      {:x (rand-int magic), :y (rand-int magic), :z (* 4 magic)}
+      (assoc star :z (* 4 magic))
       (update-in star [:z] #(- % (/ magic 25))))))
 
-(defonce updater (js/setInterval #(swap! stars move) 50))
+(def running (r/atom true))
+
+(defonce updater (js/setInterval #(when @running (swap! stars move)) 50))
 
 (defn main-component []
-  [:div
-   (for [[{:keys [x y z]} i] (map vector @stars (iterate inc 0))]
+  [:div.star-field
+   {:on-click #(do (swap! running not) nil)}
+   (for [[{:keys [x y z image class]} i] (map vector @stars (iterate inc 0))]
      (let [scale (/ magic 5 z 100)
-           size (* scale 200)
+           size (* scale 500)
            f #(+ (* (- % (/ magic 2)) scale) (- 50 (/ size 2)))]
-       [:div.star {:key (str "star-" i)
+       [:img.star {:key (str "star-" i)
+                   :class class
+                   :src image
                    :style {:left (str (f x) "vw")
                            :top (str (f y) "vh")
                            :width (str size "vw")
                            :height (str size "vw")
-                           :opacity size}}
-        " "]))])
+                           :opacity size}}]))])
 
 (defn ^:export main [el]
   (r/render-component [main-component] el))
