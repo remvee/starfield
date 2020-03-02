b4650798e3ece7db5e150b5eb166143e1101df55
Remco van 't Veer <remco@remworks.net>
2020-03-02 16:09:33 +0100

    Upgrade deps

--- project.clj
+++ project.clj
@@ -3,22 +3,23 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3211"]
-                 [reagent "0.5.0"]
-                 [figwheel "0.3.7"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.597"]
+                 [reagent "0.9.1"]]
 
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-figwheel "0.3.7"]]
+  :profiles {:dev {:plugins [[lein-cljsbuild "1.1.4"]
+                             [lein-figwheel "0.5.6"]]
+                   :dependencies [[figwheel-sidecar "0.5.19"]
+                                  [com.cemerick/piggieback "0.2.2"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]
+                                  :init (do (use 'figwheel-sidecar.repl-api) (start-figwheel!))}}}
 
-  :figwheel {:nrepl-port 7888
-             :css-dirs ["resources/public/css"]}
+  :figwheel {:css-dirs ["resources/public/css"]}
 
-  :cljsbuild {:builds {:dev {:source-paths ["dev" "src"]
-                             :compiler {:preamble ["reagent/react.js"]
-                                        :output-dir "resources/public/js-dev"
+  :cljsbuild {:builds {:dev {:source-paths ["src"]
+                             :figwheel true
+                             :compiler {:output-dir "resources/public/js-dev"
                                         :output-to "resources/public/js-dev/starfield.js"
-                                        :source-map "resources/public/js-dev/starfield.js.map"
                                         :optimizations :none}}
                        :prod {:source-paths ["src"]
                               :compiler {:output-dir "resources/public/js"
